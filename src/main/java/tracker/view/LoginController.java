8f5d7640e8b4faeb9ffa174053152830ab43ba79
Lorenzo OTTAVIANI <lorenzo.ottaviani@laplateforme.io>
2025-07-02 15:21:03 +0200

    feat: add login page display

---
+++ src/main/java/tracker/view/LoginController.java
@@ -0,0 +1,33 @@
+package tracker.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField userField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    protected void onLoginButtonClick() {
+        String username = userField.getText();
+        String password = passwordField.getText();
+
+        /* Ici, tu appelles ta méthode de vérification en base de données
+        boolean isValidUser = checkCredentials(username, password);
+
+        if (isValidUser) {
+            welcomeText.setText("You have successfully logged in!");
+        } else {
+            welcomeText.setText("Invalid username or password.");
+        } */
+    }
+
+    @FXML
+    protected void onRegisterButtonClick() {}
+}
